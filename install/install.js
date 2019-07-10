016287f5bdeca2b29a12ff1fa524f693df77c9fb
supertext <supertext@icloud.com>
2019-07-11 00:21:33 +0800

    install script

--- install/install.js
+++ install/install.js
@@ -4,6 +4,34 @@ if (!fs.existsSync(`${baseDir}/../../package.json`)) {
     console.log('is not in local project dir! no need install!');
     return
 }
+function cpdir(src, dist) {
+    fs.mkdirSync(dist);
+    var files = fs.readdirSync(src);
+    files.forEach(function (file) {
+        var srcFile = src + '/' + file;
+        var distFile = dist + '/' + file
+        if (fs.statSync(srcFile).isDirectory()) {
+            cpdir(srcFile, distFile);
+        } else {
+            fs.copyFileSync(srcFile, distFile);
+        }
+    })
+}
+var rmdir = function (dir) {
+    var files = [];
+    if (fs.existsSync(dir)) {
+        files = fs.readdirSync(dir);
+        files.forEach(function (file) {
+            var path = dir + "/" + file;
+            if (fs.statSync(path).isDirectory()) {
+                rmdir(path);
+            } else {
+                fs.unlinkSync(path);
+            }
+        });
+        fs.rmdirSync(dir);
+    }
+};
 var typesDir = baseDir + '/../@types';
 var typesTarget = typesDir + '/cmkit';
 var installDir = baseDir + '/install';
@@ -13,20 +41,6 @@ if (!fs.existsSync(typesDir)) {
 if (!fs.existsSync(typesTarget)) {
     fs.mkdirSync(typesTarget);
 }
-function copy(src, dist) {
-    fs.mkdirSync(dist);
-    var files = fs.readdirSync(src);
-    files.forEach(function (file) {
-        var srcFile = src + '/' + file;
-        var distFile = dist + '/' + file
-        if (fs.statSync(srcFile).isDirectory()) {
-            copy(srcFile, distFile);
-        } else {
-            fs.copyFileSync(srcFile, distFile);
-        }
-    })
-}
-
 if (fs.existsSync(`${baseDir}/../../project.json`) && fs.existsSync(`${baseDir}/../../assets`)) {
     var packagesDir = `${baseDir}/../../packages`;
     var packageTarget = packagesDir + '/cmkit'
@@ -34,9 +48,9 @@ if (fs.existsSync(`${baseDir}/../../project.json`) && fs.existsSync(`${baseDir}/
         fs.mkdirSync(packagesDir);
     }
     if (fs.existsSync(packageTarget)) {
-        fs.rmdirSync(packageTarget);
+        rmdir(packageTarget);
     }
-    copy(baseDir + `/cocos`, packageTarget);
+    cpdir(baseDir + `/cocos`, packageTarget);
     fs.copyFileSync(installDir + '/index.d.ts', typesTarget + '/index.d.ts')
     fs.copyFileSync(installDir + '/lib.cmkit.d.ts', typesTarget + '/lib.cmkit.d.ts')
     fs.copyFileSync(installDir + '/lib.cocos.d.ts', typesTarget + '/lib.cocos.d.ts')
